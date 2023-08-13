421a1cfe1c82d14d2c18d0ffbafb4cbe84e9e3fb
zhuzhuzihan <68634388+zhuzhuzihan@users.noreply.github.com>
2023-08-13 10:22:41 +0800

    Add files via upload

---
+++ repo.sh
@@ -0,0 +1,4 @@
+echo "https://mirrors.tuna.tsinghua.edu.cn/alpine/v3.14/main/" > /etc/apk/repositories
+echo "https://mirrors.tuna.tsinghua.edu.cn/alpine/v3.14/community/" >> /etc/apk/repositories
+echo "https://mirrors.tuna.tsinghua.edu.cn/alpine/latest-stable/community/" >> /etc/apk/repositories
+echo "https://mirrors.tuna.tsinghua.edu.cn/alpine/latest-stable/main/" >> /etc/apk/repositories
